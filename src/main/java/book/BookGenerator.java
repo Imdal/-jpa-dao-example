24e18b92326b53a54f61aab5682f81bcc7432ca3
imdal <imdal1220@gmail.com>
2020-04-29 07:53:25 +0200

    upload files

---
+++ src/main/java/book/BookGenerator.java
@@ -0,0 +1,27 @@
+package book;
+
+//import com.github.javafaker.Book;
+import com.github.javafaker.Faker;
+import book.model.Book;
+
+import java.util.Locale;
+
+public class BookGenerator {
+    private static Faker faker = new Faker(new Locale("en"));
+
+    public static Book createBook() {
+        Book book = Book.builder()
+                .isbn13(faker.code().isbn13())
+                .author(faker.book().author())
+                .title(faker.book().title())
+                .format(faker.options())
+                .publisher(faker.book().publisher())
+                .publicationDate(faker.date())
+                .pages(faker.number().numberBetween(1,500))
+                .available(faker.bool());
+        return book;
+    }
+}
+
+
+
---
+++ src/main/java/book/Main.java
@@ -0,0 +1,23 @@
+package book;
+
+import com.github.javafaker.Book;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import guice.PersistenceModule;
+;import static book.BookGenerator.createBook;
+
+public class Main {
+    public static void main(String[] args) {
+        Injector injector = Guice.createInjector(new PersistenceModule("test"));
+        BookGenerator bookGenerator = injector.getInstance(BookGenerator.class);
+        for (int i = 0; i < 1000; i++) {
+            Book book = createBook();
+            System.out.println(book);
+            bookGenerator.persist(book);
+        }
+//        bookGenerator.findAll()
+//                .stream()
+//                .forEach(System.out::println);
+    }
+
+}
